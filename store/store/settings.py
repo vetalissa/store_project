95faad832a8bed6d46f87ca172291c6d9f996b11
Elizaveta Dostovalova <lizakot20@gmail.com>
2024-04-13 17:26:20 +0300

    change bd

--- store/store/settings.py
+++ store/store/settings.py
@@ -85,12 +85,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'store.wsgi.application'
 
 # Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'roselissa_bd',
+        'USER': 'rosselissa',
+        'PASSWORD': 'privet201101',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
