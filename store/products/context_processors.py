8f677b75d5d9d25b742983c67b1bfc59634b76c2
Elizaveta Dostovalova <lizakot20@gmail.com>
2024-05-27 17:47:10 +0300

    change button for add in basket

--- store/products/context_processors.py
+++ store/products/context_processors.py
@@ -3,5 +3,6 @@ from products.models import Basket
 
 def baskets(request):
     user = request.user
-
-    return {'baskets': Basket.object.filter(user=user) if user.is_authenticated else []}
+    print()
+    return {'baskets': Basket.object.filter(user=user) if user.is_authenticated else [],
+            'basket_list': {i.product: i.quantity for i in Basket.object.filter(user=user)}}
